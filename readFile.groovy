9ad3f1e2b25cee194356b4c35672e3519f5af282
Pierre-Michel <pm.bret@worldonline.fr>
2017-06-17 15:02:31 +0200

    Utilisation d'une fonction pour la lecture

--- readFile.groovy
+++ readFile.groovy
@@ -1,4 +1,7 @@
-File input = new File('d:/temp/input01.txt')
-input.eachLine { String line ->
-    println line
+def readFile(String name){
+    new File(name).eachLine { String line ->
+        println line
+    }
 }
+
+readFile('d:/temp/input01.txt')
